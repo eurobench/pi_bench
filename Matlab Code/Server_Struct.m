cbdb22aa2965bb12c47a889e62854934cdb514f7
Sophia Tatiyosyan <sophia.tati16@gmail.com>
2019-10-23 12:40:06 +0200

    Renaming Server Client
    Confusion of the role of Server and Client, names needed to be edited : Now the new Client is the old Server Script and the other way around

--- Matlab Code/Server_Struct.m
+++ Matlab Code/Server_Struct.m
@@ -1,82 +1,62 @@
-%% Main script: Data Server
-% Server: receiving data
-clear 
+%% Main script: Server 
+% Server: sending data
+clear
 close all
 
-global streaming 
-streaming = 0;
-NumPackReceived = 0;
+% set up TCP IP connection for Client
+% enter IP adress of the computer that will send 
+%t= tcpip('localhost',30000,'NetworkRole','client');
+t= tcpip('129.69.168.20',30000,'NetworkRole','client');
 
-% set up TCP IP connection for Data Server
-t = tcpip('0.0.0.0', 30000, 'NetworkRole', 'server');
+% % initialize ROS node to retrieve time ROS-compatible
+% rosinit;
 
-% initialize ROS node to retrieve time ROS-compatible
-rosinit;
+% collect and send data struct made from timestamp, values and additional information
+    data = struct('time_pro',[],'values',[],'information',[]);
+    %size_data = 784; % in bytes %timestamp + values + info
+    size_data = 776; % without rostime
+    t.OutputBufferSize = size_data;
 
-% get information about size of data that will be arriving
-size_data = 784; %timestamp + values + info 
-t.InputBufferSize = size_data;
-
-% create data struct 
-datastruct = struct('time_pro_sec',[],'time_pro_nsec',[], 'values',[],'information',[],'time_rec_sec',[],'time_rec_nsec',[]);
-
-% GUI for starting streaming 
-uf = figure;
-uf.Name = 'Streaming of gathered data';
-uf.Position = [500 300 400 175];
-
-% start streaming button
-uicontrol('Style', 'pushbutton', 'Position', [50 75 125 50], 'String', 'Start Streaming', 'Callback', @StartButton);
-% stop straming button
-uicontrol('Style', 'pushbutton', 'Position', [225 75 125 50], 'String', 'Stop Streaming', 'Callback', @StopButton);
-
-while streaming == 0 
-    pause(0.1)
-end
-
-if streaming == 1
-    fopen(t);
-
-    % receive data (local function)
-    disp('Waiting for data to arrive')
-
-    while t.BytesAvailable == 0
-        pause(0.1)
-    end
-    % data arrives in a single column
-    % Size*Precision must be equal to InputBufferSize 
     
-disp('Data arriving')
+disp('Trying to send data...')
 
-    while streaming ==1
-        data = fread(t, size_data/8, 'double'); %divided by 8 as double has 8 Byte
-        time_rec = rostime('now');
- 
-        %seperate data into struct
-        datastruct.time_pro_sec = data(1);
-        datastruct.time_pro_nsec = data(2);
-        datastruct.values = data(3:34);
-        datastruct.information = char(data(35:end));
-        datastruct.time_rec_sec = time_rec.Sec;
-        datastruct.time_rec_nsec = time_rec.Nsec;
-
-        pause(0.01)
-        
-        if isempty(data)
-            disp('No new data received')
-        else
-            data_storage(NumPackReceived+1) = datastruct;
-            NumPackReceived = NumPackReceived +1;
+NumPackSent = 0;
+error = 0;
+    
+    while (strcmp(t.Status, 'closed'))
+        try 
+            fopen(t);
+            disp('TCP IP connection established')
+        catch
+            disp('No server listening.')
+            pause(0.5)
+        end
+    end 
+    
+    while (strcmp(t.Status, 'open')) %&& strcmp(s.Status, 'closed'))
+        try
+            %data.time_produced = rostime('now');
+            %timeSec = double(data.time_produced.Sec);
+            %timeNsec = double(data.time_produced.Nsec);
+            data.time_pro = posixtime(datetime('now'));
+            data.values = randi(100,32,1, 'double');
+            data.information = 'no info no info no info no info no info no info no info no info ';
+            data_store(NumPackSent+1)= data;
+            %data_to_send = vertcat( timeSec , timeNsec, data.values, double(data.information)');
+            data_to_send = vertcat( data.time_pro, data.values, double(data.information)');
+            fwrite(t, data_to_send, 'double');
+            %pause(0.0001)
+            NumPackSent = NumPackSent + 1;
+        catch
+            disp('Server not listening anymore')
+            break
         end
     end
-end
-if streaming == 2
-    disp('Data arrived')
-    save data_storage
-    pause(0.5)
-    %end data communication
-    fclose(t);
-end
 
-% close ros node
-rosshutdown
+
+% tell whether data sending was successfull
+disp('Data sent successfully')
+disp(['Number of sent packs:', num2str(NumPackSent)])
+ 
+% % close ros node
+% rosshutdown
