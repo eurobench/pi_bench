68e73f0cdab7eb52f267eb952c1136f81a69b56c
Sophia Tatiyosyan <sophia.tati16@gmail.com>
2020-05-07 10:30:00 +0200

    Clean up

--- Matlab Code/StopButton.m
+++
@@ -1,9 +0,0 @@
-function StopButton(~,~)
-%STOPBUTTON 
-%   Sets the global streaming variable to 2 to stop streaming process
-global streaming
-streaming = 2;
-disp('Stop button pushed...streaming will stop.')
-end
-
-
