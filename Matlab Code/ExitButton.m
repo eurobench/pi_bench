bd19f09f9289e820afd183fd3e2360339b764ef9
sophiaanais <sophia.tati16@gmail.com>
2020-05-07 10:23:31 +0200

    Clean up

--- Matlab Code/ExitButton.m
+++
@@ -1,9 +0,0 @@
-function ExitButton(~,~)
-%EXITBUTTON 
-%   Sets the global streaming variable to 3 to stop streaming and close the
-%   connection clientwise
-global streaming
-streaming = 3;
-disp('Exit button pushed...script will stop running.')
-end
-
