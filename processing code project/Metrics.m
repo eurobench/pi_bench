15fe490d23a75dccb7e910f7f182ded6564cc3af
lgizzi <gizzi@mechbau.uni.stuttgart.de>
2020-06-19 18:13:32 +0200

    implemented overshoot :)

--- processing code project/Metrics.m
+++ processing code project/Metrics.m
@@ -17,27 +17,32 @@ fprintf('we need to read anthropometric data, but the fucntion is not ready yet!
 end
 
 
-function [FP1, FP2, accel,back_sensor] = read_sensor_data(data_file) %how is the sensor data arranged?
+function [FP1, FP2, handrail_L, handrail_R,back_sensor, accel] = read_sensor_data(data_file) %how is the sensor data arranged?
 %Preliminary indexing
 ind_plate_1 = 1:6;
 ind_plate_2 = 7:13;
-ind_acc = 20:40; %MADE UP!!!!
+ind_handrail_L = 14; %made up
+ind_handrail_L = 15; %made up
+
 ind_back  = 51; %MADE UP!!!
+ind_acc = 20:40; %MADE UP!!!!
 
 %load .CSV
-temp_data = importdataI(data_file); %check if it is the right one
+temp_data = importdataI(data_file);
 
 %parse data
 FP1 = temp_data(:,ind_plate_1);
 FP2= temp_data(:,ind_plate_2);
-accel = temp_data(:,ind_acc);
+handrail_L  = temp_data(:,ind_handrail_L);
+handrail_R  = temp_data(:,ind_handrail_R);
 back_sensor = temp_data(:, ind_back);
+accel = temp_data(:,ind_acc);
 end
 
 
 
 
-function [start,stop] = segmentation(FP1, FP2,duration, Fs)
+function [start,stop] = segmentation(FP1, FP2,duration, Fs) %incomplete
 %provides the start and stopping frame of the current session
 %input data are the forces from the plates
 %the output are the starting and stopping frame of the current session
@@ -66,7 +71,7 @@ end
 %Implementation: seat stability is the distance traveled by the CoP on a 10s period after start, (normalized by the subject's height?). Stand stability is the same, in standing position, before stop.
 
 % -----------------------------------------------------------------------------------------------------------------------------------
-function [joint_angles] = kinematics(IMUs_markers, start,stop)
+function [joint_angles] = kinematics(IMUs_markers, start,stop) %to be done
 joint_angles = [];
 fprintf('The subphases have not been computed yet')
 
@@ -77,7 +82,7 @@ fprintf('The subphases have not been computed yet')
 end
 
 % -----------------------------------------------------------------------------------------------------------------------------------
-function [subphases] = segment_STS(start,stop,joint_angles,FPs)
+function [subphases] = segment_STS(start,stop,joint_angles,FPs) %to be done
 subphases = [];
 fprintf('The subphases have not been computed yet')
 
@@ -90,26 +95,50 @@ fprintf('The subphases have not been computed yet')
 end
 
 % -----------------------------------------------------------------------------------------------------------------------------------
- function [time] = sts_duration(start,stop,subphases)
+ function [time] = sts_duration(start,stop,subphases) %to be done
 %determines the duration for the 5STS protocol
 % Starting from the identified subphases, calculates the time elapsed between the t0 of the first sit-to-stand and the end of the 5th stand-to-seat (technically this should correspond, in 5sts literature, to the buttocks contact with the chair, and not the complete seat stabilization)
  end
  
 % -----------------------------------------------------------------------------------------------------------------------------------
-function [number_of_sts] = sts_counter(initial_frame, last_frame, subphases)
+function [number_of_sts] = sts_counter(initial_frame, last_frame, subphases) %to be done
 %counts the number of completed sts cycles between the two input frames. In the case of the 30s STS, only the initial_frame is needed, and it correspond to the â€œgoâ€? signal. Last frame will be initial_frame+30s
 % Implementation: simply counts the number of completed cycles.
 end
 
 % -----------------------------------------------------------------------------------------------------------------------------------
-function [load_transf] = load_transfer(input)
+function [load_transf] = load_transfer(FP1, FP2) %to be done
+% Unidirectional Load transfer time 
+%definition: the time betwee the beginning of the movement and the moment
+%the body touches or leaves the seat plate
 load_transf = [];
 fprintf('This function computes the load transfer and is not implemented yet')
 
 end
 % -----------------------------------------------------------------------------------------------------------------------------------
- function [os] = overshoot(input)
- os = [];
+ function [os] = overshoot(AP, start_ind, CoPx) %to be tested
+ %definition: the overshoot is defined as the distance travelled by the
+ %CoP after it has passed the vertical stance, and until it starts coming back
+ %to the vertical stance
+ %vertical stance is defined as the position of CoP when AP == 0 (where AP 
+ %is the force in the the antero-posterior)
+ 
+ %start_ind is inherited from phases segmentation?
+
+ %ignore anything that happened before the start
+ AP(1:start_ind) = NaN; 
+
+ %find AP == 0 for the first time --> index of vertical stance
+ t = find(AP==0);
+ vert_ind = t(1); clear (t)
+%ignore anything that happened before verd_ind
+ AP(1:vert_ind) = NaN; 
+ 
+ 
+ %find diff(AP) = 0 --> index of direction change
+ t = diff(AP); inv = find(t==0); 
+ first_inversion = inv(1); %find the first inversion
+ os =abs( CoPx(vert_ind)- CoPx(first_inversion));
  fprintf('This function computes the overshoot and is not implemented yet')
  end
 % -----------------------------------------------------------------------------------------------------------------------------------
@@ -147,7 +176,7 @@ function [stability] = compute_stability(FP1, FP2,start,stop)
 %provides the stability values. The same function is called once for each
 %subphases of each cycle.
 %input data are the forces from the plates and the start/stop frames from the segmentation function
-%the outputs are the sit and stand stability values
+%the outputs is the stability value, calculated as the CoP travel distance normalized by height 
 stability = [];
 fprintf('The stability has not been computed yet')
 end
